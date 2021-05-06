399b9ebc1be4607b8d091fdb3c94105d95247053
doublefreeman <317471917@qq.com>
2021-05-06 22:49:59 +0800

    add temp

--- updatenumber.sh
+++ updatenumber.sh
@@ -1,7 +1,10 @@
 cd /home/pi/number
 git pull
+rm -f time.txt
+rm -f temp.txt
 curl -g http://114.118.7.90:1080/Push/message.do -o data.txt
 echo `date '+%Y%m%d-%H%M%S'`>>time.txt 
+echo `vcgencmd measure_temp`>>temp.txt
 git add .
 git commit -m "update" -a
 git push
